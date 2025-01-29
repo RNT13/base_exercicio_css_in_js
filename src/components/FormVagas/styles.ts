97d65f041df599348ebd8af0666b84e9cc9aaed8
RNT13 <renatornt13@gmail.com>
2025-01-29 14:45:41 -0300

    correção do layout responsivo

--- src/components/FormVagas/styles.ts
+++ src/components/FormVagas/styles.ts
@@ -7,21 +7,43 @@ export const FormContainer = styled.form`
   padding: 32px;
   border-radius: 12px;
   margin-top: 40px;
+  gap: 8px;
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 export const InputField = styled.input`
-  padding: 0 16px;
+  padding: 12px 16px;
+  font-size: 16px;
+  border-radius: 8px;
+  border: 1px solid ${(props) => props.theme.primaryColor};
   outline-color: ${(props) => props.theme.primaryColor};
+  width: 100%;
+
+  @media (max-width: 767px) {
+    font-size: 14px;
+  }
 `
 
 export const SearchButton = styled.button`
   background-color: ${(props) => props.theme.primaryColor};
-  border: 1px solid ${(props) => props.theme.primaryColor};
+  border: none;
   height: 40px;
   padding: 0 16px;
   font-size: 18px;
   color: ${(props) => props.theme.secondaryColor};
-  margin-left: 8px;
+  border-radius: 8px;
   cursor: pointer;
   transition: all 0.3s ease;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  @media (max-width: 767px) {
+    font-size: 16px;
+    height: 36px;
+  }
 `
--- src/containers/ListaVagas/styles.ts
+++ src/containers/ListaVagas/styles.ts
@@ -6,20 +6,36 @@ export const Container = styled.div`
   box-sizing: border-box;
   margin: 0 auto;
 
-  @media (max-width: 768px) {
-    max-width: 80%;
+  @media (max-width: 767px) {
+    max-width: 95%;
+  }
+
+  @media (min-width: 768px) and (max-width: 1023px) {
+    max-width: 90%;
+  }
+
+  @media (min-width: 1024px) {
+    max-width: 85%;
   }
 `
 
 export const VagasList = styled.ul`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
   column-gap: 16px;
   row-gap: 16px;
   margin-top: 32px;
   list-style: none;
 
-  @media (max-width: 768px) {
+  @media (max-width: 767px) {
     grid-template-columns: 1fr;
+    margin-bottom: 32px;
+  }
+
+  @media (min-width: 768px) and (max-width: 1023px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `
